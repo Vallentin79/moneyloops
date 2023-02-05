5d541ef076c02ccd700701ace04391c25e7bdb8d
Vallentin79 <johnvallentin@hotmail.com>
2023-02-05 12:26:52 +0100

    wip

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -6,9 +6,12 @@ import { Questions } from '../data/data.interfaces';
 function QuestionsPage() {
     const [getQ, setQ] = useState<Questions>({
         Investment: 1000,
-        Goals: 1,
-        Interact: 1
+        Goals: 1,  
+        Interact: 1     
     });
+     // 
+    const [showResult, setResult] = useState(false);
+
 
     const GoalsText = [
         "Funds 100% accessible. Nothing locked. Lowest returns and risk.",
@@ -25,6 +28,7 @@ function QuestionsPage() {
     ]
 
     const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        //setResult(false);
         const element = e.target;
         const value = element.type == 'checkbox' ? element.checked : parseInt(element.value) | 0;
         setQ({ ...getQ, [element.name]: value });
@@ -61,23 +65,41 @@ function QuestionsPage() {
                 <Row xs={1} sm={1} lg={1} xl={2} xxl={2}>
                     <Col className='answer'><span style={{ color: 'white' }}>Answer:</span> {InteractText[getQ.Interact]}</Col>
                 </Row>
-                <Row xs={1} sm={1} lg={2} xl={3} xxl={3}>
-                    <Col style={{ paddingTop: 4 }}><input name="Interact" type="range" min="0" max="3" step="1" value={getQ.Interact} onChange={inputChange} className="slider" /></Col>
+                <Row xs={3} sm={3} lg={2} xl={3} xxl={3}>
+                    <Col style={{ paddingTop: 4 }}><input name="Interact" type="range" min="0" max="1" step="0" value={getQ.Interact} onChange={inputChange} className="slider" /></Col>
                 </Row>
             </Container>
             <Container fluid style={{ marginTop: 40 }}>
                 <Row xs={1} sm={1} lg={1} xl={1} xxl={2}>
                     <Col>
-                        <button className="button" type="button">
+                        <button onClick={() => getResult()} className="button" type="button">
                             <span className="buttonText">Get My Strategy</span>
                         </button>
                     </Col>
                 </Row>
             </Container>
-
+            {showResult ?
+                <div>
+                    <br />
+                    <span>Investment {getQ.Investment} </span>
+                    <br />
+                    <span>Goal {getQ.Goals} </span>
+                    <br />
+                    <span>Risk {getQ.Interact} </span>
+                </div>
+                : null}
             {/* <button onClick={() => console.log("q", getQ)}>click</button> */}
         </ThemeProvider>
     );
+
+
+    function getResult() {
+        setResult(true);
+        console.log(getQ);
+
+    }
 }
+
+
 
 export default QuestionsPage;
